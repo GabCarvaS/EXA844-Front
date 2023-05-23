699a60906049d8797fd40a56dc8dcdbe06e6c49d
Gabriel <gabrielcfacul@gmail.com>
2023-05-22 22:50:37 -0300

    criacao da home

--- EXA844-front/src/routes/Home.jsx
+++ EXA844-front/src/routes/Home.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import blogFetch from "../axios/config";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/month?month=Abril");
+
+      const data = response.data;
+
+      setPosts(data);
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Carros Abril</h1>
+      {posts.length == 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.car.posicao}>
+            <h2>{post.car.modelo}</h2>
+            <p>{post.car.marca}</p>
+            <Link to={`/posts/${post.car.posicao}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
